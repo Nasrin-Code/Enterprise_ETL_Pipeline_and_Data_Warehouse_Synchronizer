4d4dccb7883d6f23040812a5a76ea32a50aa29ef
Nasrin A <nasrin512003@gmail.com>
2026-08-15 16:06:01 +0530

    feat: add pydantic models and settings

---
+++ src/config.py
@@ -0,0 +1,5 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+class Settings(BaseSettings):
+    stripe_api_key: str
+    model_config = SettingsConfigDict(env_file = ".env")
---
+++ src/models.py
@@ -0,0 +1,6 @@
+from pydantic import BaseModel
+
+class Customer(BaseModel):
+    customer_id: int
+    name: str
+    email: str
